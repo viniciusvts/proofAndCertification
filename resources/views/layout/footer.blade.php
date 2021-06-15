08a7ef13bf0577d6424aa02b46215f0ea582716c
Vinicius de Santana <vinicius.vts@gmail.com>
2021-06-15 11:47:55 -0300

    fix: activate rdstation script

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,4 @@
 <!--<body> in header-->
-	<!--script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/66bbf119-95bf-48e1-a7e1-3626378a293f-loader.js" ></script-->
+	<script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/66bbf119-95bf-48e1-a7e1-3626378a293f-loader.js" ></script>
 </body>
 </html>

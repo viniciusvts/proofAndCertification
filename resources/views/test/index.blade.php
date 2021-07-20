405382107c5e6723b7cd4450c21d72b9f0e8cd07
Vinicius de Santana <vinicius.vts@gmail.com>
2021-07-20 15:24:20 -0300

    fix: gramatic

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -13,7 +13,7 @@
     <div class="row test-list">
       <div class="col-12 white-box">
         <h3>{{ $test->title }}</h3>
-        <p>{{ count($test->questions) }} questôes, {{ $test->time_to_finish }} minutos para concluir.</p>
+        <p>{{ count($test->questions) }} questões, {{ $test->time_to_finish }} minutos para concluir.</p>
         <div class="d-flex">
           @if ($isAdmin)
           <form action="{{ route('test.destroy', ['id' => $test->id]) }}" method="POST">
